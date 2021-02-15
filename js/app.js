da53d15ff0bbd1721c813c7e2b19d3ab02a14241
areejobaid94 <areej.obaid@yahoo.com>
2021-02-15 19:29:44 +0200

    done

--- js/app.js
+++ js/app.js
@@ -1,44 +1,102 @@
 "use strict";
 let arr = [];
+let allArr = [];
+let selectedArr = [];
+let selectedKey;
+let selectedSort;
+
 function card(ele) {
     this.keyword = ele.keyword;
     this.image_url = ele.image_url;
     this.title = ele.title;
     this.description = ele.description;
     this.horns = ele.horns;
+    allArr.push(this);
 }
 
 card.prototype.render = function () {
     let card = $("#photo-template").clone();
     card.find("img").attr("src", this.image_url);
-    console.log(this.image_url);
     card.find("h2").text(this.title);
-    card.find("p").text(this.description);
-    $("main").append(card);
-    card.toggleClass(this.keyword);
+    card.find("h3").text(`Description: ${this.description}`);
+    card.find(".keyword").text(`Keyword: ${this.keyword}`);
+    card.find(".horns").text(`Horns: ${this.horns}`);
+    $("#allItems").append(card);
 }
 
 $(document).ready(() => {
     $.ajax({ url: "data/page-1.json", dataType: 'json' }).then(function (data) {
-        data.forEach(element => {
-            let newCard = new card(element);
-            if(arr.includes(newCard.keyword) == false){
-                arr.push(newCard.keyword);
-                newCard.appendSelector();
-            }
-            newCard.render();
-        });
+        showAll(data, false);
+        selectedArr = allArr.slice();
     });
-    $("section").show();    
-    $('select').on('change', function (e) {
-        var valueSelected = this.value;
-        console.log(valueSelected);
-        $("section").hide();
-        $(`.${valueSelected}`).show();
+    $('#select').on('change', function (e) {
+        selectedKey = this.value;
+        updateSelectedArr(selectedKey)
+        sortBy();
     });
-    
+
+    $('#sort-by').on('change', function (e) {
+        selectedSort = this.value;
+        if (selectedSort != 'default' && selectedSort != undefined) {
+            sortBy();
+            $('button').css('color', 'gray');
+            $(`#quick-sort-${selectedSort}`).css('color', 'red');
+        }
+    });
+    $('#quick-sort-horns').on('click', function (e) {
+        e.preventDefault();
+        selectedSort = 'horns';
+        updateSelectedArr();
+        $('button').css('color', 'gray');
+        $(this).css('color', 'red');
+        sortBy();
+        $("#sort-by").val('horns');
+    })
+    $('#quick-sort-title').on('click', function (e) {
+        e.preventDefault();
+        selectedSort = 'title';
+        updateSelectedArr();
+        $('button').css('color', 'gray');
+        $(this).css('color', 'red');
+        sortBy();
+        $("#sort-by").val('title');
+    })
 })
 
 card.prototype.appendSelector = function () {
     $("#select").append(new Option(this.keyword, this.keyword));
+}
+
+function sortBy() {
+    if (selectedSort != undefined || selectedSort != 'default') {
+        selectedArr = selectedArr.sort((a, b) => (a[selectedSort] > b[selectedSort]) ? 1 : (a[selectedSort] === b[selectedSort]) ? ((a[selectedSort] > b[selectedSort]) ? 1 : -1) : -1)
+    }
+    showAll(selectedArr, true);
+}
+
+function showAll(selArr, isNotFirstTime) {
+    $('#allItems').empty();
+    selArr.forEach(element => {
+        let newCard;
+        isNotFirstTime == false ? newCard = new card(element) : newCard = element;
+        if (arr.includes(newCard.keyword) == false) {
+            arr.push(newCard.keyword);
+            newCard.appendSelector();
+        }
+        newCard.render();
+    });
+}
+
+function updateSelectedArr(val) {
+    if (val != undefined && val != 'default') {
+        selectedArr = [];
+        allArr.forEach(ele => {
+            if (ele['keyword'] === val) {
+                selectedArr.push(ele);
+            }
+        });
+    }else{
+        selectedArr = allArr.slice();
+    }
+    console.log(selectedArr)
 }
